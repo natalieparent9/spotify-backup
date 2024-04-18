48e59112537a9fd6231aa2492ac0b58d787023e4
natalieparent9 <78802736+natalieparent9@users.noreply.github.com>
2024-04-18 21:56:29 +0930

    Load packages and tokens

--- spotify backup.py
+++ spotify backup.py
@@ -1,7 +1,33 @@
 
-import spotipy
+# pip3 install spotipy
 
+import spotipy
 from dotenv import load_dotenv
+import os
+import spotipy.util as util
+import sys
+
 from spotipy.oauth2 import SpotifyClientCredentials
 
+scope = 'user-library-read'
 
+load_dotenv()
+client_id = os.getenv('SPOTIPY_CLIENT_ID')
+client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
+client_redirect = os.getenv('SPOTIPY_REDIRECT_URI')
+
+# client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+
+util.prompt_for_user_token(username='natalieparent9',
+                           scope=scope,
+                           client_id=client_id,
+                           client_secret=client_secret,
+                           redirect_uri=client_redirect)
+
+
+results = sp.current_user_saved_tracks()
+
+print(sys.argv)
